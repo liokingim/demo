c7abe8c0b76f8f90bef76626278463939dc3ce1e
liokingim <liokingim@gmail.com>
2024-01-16 14:22:23 +0900

    vsc 자바 설정

---
+++ src/main/java/shop/coding/demo/IndexController.java
@@ -0,0 +1,23 @@
+package shop.coding.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        log.debug("디버그");
+        log.info("인포");
+        log.warn("경고");
+        log.error("에러");
+
+        return "index ok";
+    }
+
+}
